d208535a6a0ffd73ec6d8176689684ebda690fca
Deivid da Silva G <deivid.sgalvao@hotmail.com>
2023-03-07 21:28:38 -0300

    Create Principal.java

---
+++ Principal.java
@@ -0,0 +1,16 @@
+
+//package aula2;
+
+public class Principal {
+    
+    public static void main(String args[] ) {
+    
+        Calculadora calculadora = new Calculadora();
+        calculadora.setCampo1(10);
+        calculadora.setCampo2(20);
+        calculadora.setOperacao(1);
+        calculadora.imprimir();
+        
+    }
+    
+}
