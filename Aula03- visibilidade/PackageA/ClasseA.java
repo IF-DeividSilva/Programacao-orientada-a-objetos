de666cb89cac5e33254013e93e831fae94bb4756
Deivid da Silva G <deivid.sgalvao@hotmail.com>
2023-03-17 11:53:03 -0300

    Create ClasseA.java

---
+++ Aula03- visibilidade/PackageA/ClasseA.java
@@ -0,0 +1,15 @@
+package PackageA;
+
+public class ClasseA{
+    
+    public ClasseA(){
+        Principal p = new Principal();
+        
+        System.out.println(p.primeiro);
+        System.out.println(p.segundo);
+        
+        System.out.println(p.terceiro);
+       // System.out.println(p.quarto;
+    }
+    
+}
