61a524edcc453fe6312a42be1fc27e323f598cb7
Deivid da Silva G <deivid.sgalvao@hotmail.com>
2023-03-17 11:52:56 -0300

    Create Principal.java

---
+++ Aula03- visibilidade/PackageA/Principal.java
@@ -0,0 +1,13 @@
+
+package PackageA;
+
+public class Principal {
+     
+    public int primeiro; // todos tem acesso
+    int segundo;
+    protected int terceiro; // dentro do mesmo package ou filhos
+    private int quarto; // niguem tem acesso
+    public static void main(String[] args) {
+        
+    }
+}
