fa67af94e9fce45fecbaa11911c37fd0487d4e87
Deivid da Silva G <deivid.sgalvao@hotmail.com>
2023-04-01 18:37:49 -0300

    Create Principal.java

---
+++ Trabalho1/Trabalho1/Principal.java
@@ -0,0 +1,15 @@
+package Trabalho1;
+
+import java.util.Scanner;
+import java.time.format.DateTimeFormatter;
+import java.time.LocalDateTime;
+
+public class Principal {
+    public static void main(String[] args)  {   
+        
+        // instanciando duas padarias
+        Padaria padariaA = new Padaria();
+        Padaria padariaB = new Padaria();
+        
+    }
+}
