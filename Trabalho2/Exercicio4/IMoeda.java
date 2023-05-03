157000a14237958eee8e6ce9689a534223df815a
Deivid da Silva G <deivid.sgalvao@hotmail.com>
2023-05-03 16:47:55 -0300

    Create IMoeda.java

---
+++ Trabalho2/Exercicio4/IMoeda.java
@@ -0,0 +1,10 @@
+package exercicio4;
+/*
+Exercicio <4>
+Autor(es): <Deivid da Silva Galvao> e <JOAO VITOR NAKAHODO YOSHIDA>
+Data: <01/05/2023>
+*/
+public interface IMoeda {
+    public abstract void formaPagamento();
+}
+
