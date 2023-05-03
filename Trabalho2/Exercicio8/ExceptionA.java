964768cfd3c3f152ca084ba4583e3693642766da
Deivid da Silva G <deivid.sgalvao@hotmail.com>
2023-05-03 17:51:55 -0300

    Create ExceptionA.java

---
+++ Trabalho2/Exercicio8/ExceptionA.java
@@ -0,0 +1,11 @@
+package exercicio.pkg8;
+/*
+Exercicio <8>
+Autor(es): <Deivid da Silva Galvao> e <JOAO VITOR NAKAHODO YOSHIDA>
+Data: <28/04/2023>
+*/
+public class ExceptionA extends Exception {
+    public ExceptionA(String m){
+        super(m);
+    }
+}
