0018ad4642708f7f6ea0998227043914a3da5724
Deivid da Silva G <deivid.sgalvao@hotmail.com>
2023-05-03 17:51:48 -0300

    Create Subtracao.java

---
+++ Trabalho2/Exercicio7/Subtracao.java
@@ -0,0 +1,40 @@
+package exercicio7;
+/*
+Exercicio <7>
+Autor(es): <Deivid da Silva Galvao> e <JOAO VITOR NAKAHODO YOSHIDA>
+Data: <28/04/2023>
+*/
+abstract class Subtracao implements IOperacoes {
+    private float op1;
+    private float op2;
+    private static int quantidadeInstancias;
+   
+    
+    public Subtracao(){
+        quantidadeInstancias = quantidadeInstancias + 1;
+    }
+    
+    @Override 
+    public void setOperando1(float op1){
+         this.op1 = op1;
+    }
+    @Override 
+    public void setOperando2(float op2){
+         this.op2 = op2;
+    }
+    @Override
+    public float getResultado(){
+        return op1 - op2;
+    }
+    @Override
+    public String getNomeOp(){
+        return "Subtracao";
+    }
+    @Override
+    public int getQuantidadeInstancias(){
+        return quantidadeInstancias;
+    }
+    
+    
+
+}
