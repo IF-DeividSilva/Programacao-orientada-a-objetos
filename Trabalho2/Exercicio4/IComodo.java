bea17b280c476c66e700fc9bff04f555b2c99b3b
Deivid da Silva G <deivid.sgalvao@hotmail.com>
2023-05-03 16:47:52 -0300

    Create IComodo.java

---
+++ Trabalho2/Exercicio4/IComodo.java
@@ -0,0 +1,11 @@
+package Exercicio4;
+/*
+Exercicio <4>
+Autor(es): <Deivid da Silva Galvao> e <JOAO VITOR NAKAHODO YOSHIDA>
+Data: <<01/05/2023>>
+*/
+public interface IComodo {
+    public abstract String emitirComanda(String pedido);
+    public abstract String calcularPagamento(String pedido);
+    public abstract void calcularCompras(int i);
+}
