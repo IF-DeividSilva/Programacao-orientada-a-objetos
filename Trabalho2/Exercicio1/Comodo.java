4556b1479fea5c19c823d48ae3e303d0a927c9ae
Deivid da Silva G <deivid.sgalvao@hotmail.com>
2023-05-03 16:46:53 -0300

    Create Comodo.java

---
+++ Trabalho2/Exercicio1/Comodo.java
@@ -0,0 +1,21 @@
+/*
+Exercicio <1>
+Autor(es): <Deivid da Silva Galvao> e <JOAO VITOR NAKAHODO YOSHIDA>
+Data: <27/04/2023>
+*/
+
+
+package lista.pkg2;
+
+public class Comodo {
+    private String nome; 
+    
+    public Comodo(String nome){
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+    
+}
