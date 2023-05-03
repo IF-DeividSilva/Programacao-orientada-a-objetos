db9b9d62a3472911dde2ca8a672479cbf78c2412
Deivid da Silva G <deivid.sgalvao@hotmail.com>
2023-05-03 17:51:34 -0300

    Create IOperacoes.java

---
+++ Trabalho2/Exercicio7/IOperacoes.java
@@ -0,0 +1,13 @@
+package exercicio7;
+/*
+Exercicio <7>
+Autor(es): <Deivid da Silva Galvao> e <JOAO VITOR NAKAHODO YOSHIDA>
+Data: <28/04/2023>
+*/
+public interface IOperacoes {
+    void setOperando1(float op1);
+    void setOperando2(float op2);
+    float getResultado();
+    String getNomeOp();
+    int getQuantidadeInstancias();
+}
