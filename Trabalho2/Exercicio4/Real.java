4142e463e8137dd7fac790f2be0b8d437c040017
Deivid da Silva G <deivid.sgalvao@hotmail.com>
2023-05-03 16:48:10 -0300

    Create Real.java

---
+++ Trabalho2/Exercicio4/Real.java
@@ -0,0 +1,14 @@
+package exercicio4;
+/*
+Exercicio <4>
+Autor(es): <Deivid da Silva Galvao> e <JOAO VITOR NAKAHODO YOSHIDA>
+Data: <01/05/2023>
+*/
+public class Real implements IMoeda {
+
+    @Override
+    public void formaPagamento() {
+        System.out.println("Pagamento em Real");   
+    }
+    
+}
